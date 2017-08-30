319815b4b914588f0a6caec8d4bf7fab2d79fc19
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-30 22:43:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_PNmMagekPV",
+  "homePage" : "Main",
+  "name" : "Auto_PNmMagekPV",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
